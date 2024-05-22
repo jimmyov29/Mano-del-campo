594eb54bae16597772baa96eaac81e19423d0673
Jimmy Oliva <jimmy.oliva@cit.hn>
2024-05-22 00:10:28 -0600

    fix (banner): imagen centrada

--- src/app/components/Feed/Feed.tsx
+++ src/app/components/Feed/Feed.tsx
@@ -3,19 +3,22 @@ import Image from "next/image";
 
 export const Feed = () => {
   return (
-    <section className=" p-6">
-      <article className=" space-y-4">
-        <h1 className=" text-center font-extrabold text-5xl">
-          Unete a la comunidad de consumidores que valoran la agricultura
+    <section className="p-6">
+      <article className="space-y-4">
+        <h1 className="text-center font-extrabold text-5xl">
+          Únete a la comunidad de consumidores que valoran la agricultura
           sostenible y responsable.
         </h1>
-        <figure className="relative w-auto h-96 rounded-2xl overflow-hidden">
-          <Image
-            src={"/assets/banner/banner1.webp"}
-            alt={"banner"}
-            fill
-          ></Image>
-        </figure>
+        <div className="w-full flex justify-center">
+          <figure className="relative w-full h-96 rounded-2xl overflow-hidden max-w-[1200px]">
+            <Image
+              src={"/assets/banner/banner1.webp"}
+              alt={"banner"}
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </figure>
+        </div>
       </article>
     </section>
   );
