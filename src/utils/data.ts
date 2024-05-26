3ba0a64f530535178e648e97eeb56cd4f7256070
Jimmy Oliva <jimmy.oliva@cit.hn>
2024-05-26 16:59:45 -0600

    feat (core): utils with data

---
+++ src/utils/data.ts
@@ -0,0 +1,41 @@
+import { Product } from "@/app/interfaces/Products.types";
+
+export const Products:Product[] = [
+    {
+        name: "Sandia",
+        companyName: "Del campo Jose",
+        imagePath: "/assets/products/sandia.png",
+        price: 25,
+    },
+    {
+        name: "Cilantro",
+        companyName: "Juan Ramirez",
+        imagePath: "/assets/products/cilantro.png",
+        price: 5,
+    },
+    {
+        name: "Melon",
+        companyName: "Del campo Jose",
+        imagePath: "/assets/products/melon.png",
+        price: 30,
+    },
+    {
+        name: "Yuca",
+        companyName: "Don Pancho",
+        imagePath: "/assets/products/yuca.png",
+        price: 25,
+    },
+    {
+        name: "Papa",
+        companyName: "Angie",
+        imagePath: "/assets/products/papa.png",
+        price: 14,
+    },
+    {
+        name: "Tomate",
+        companyName: "Del campo de Jose",
+        imagePath: "/assets/products/tomate.png",
+        price: 25,
+    }
+    
+]
